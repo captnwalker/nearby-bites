7854e23d726efb35ca5cb402d565f0a8b4c6ce32
Steve Walker <captnwalker@gmail.com>
2026-09-23 17:52:04 -0400

    Bump service worker cache so browsers drop the broken JS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nearby-bites-v1";
+const CACHE = "nearby-bites-v2";
 const SHELL = ["./", "./index.html", "./css/app.css", "./js/app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
