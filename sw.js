54239e652eb21acbfc7e894cbe427ad25f748552
Steve Walker <captnwalker@gmail.com>
2026-09-23 17:19:53 -0400

    Add service worker

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "nearby-bites-v1";
+const SHELL = ["./", "./index.html", "./css/app.css", "./js/app.js", "./manifest.webmanifest"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.hostname.includes("overpass") || url.hostname.includes("nominatim") || url.hostname.includes("tile.openstreetmap")) {
+    return;
+  }
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(event.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(event.request).then((hit) => hit || caches.match("./index.html")))
+  );
+});
